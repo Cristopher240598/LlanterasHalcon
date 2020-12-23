949fadb122a022410ae74f86e34458f7616feadc
pablofranco12 <walliwest12@gmail.com>
2020-12-23 17:50:36 -0600

    commit pablo

--- Proyecto_1/Models/Modelo.Designer.cs
+++ Proyecto_1/Models/Modelo.Designer.cs
@@ -1,4 +1,4 @@
-﻿// La generación de código T4 está habilitada para el modelo 'D:\OneDrive - Instituto Tecnológico de Toluca\Cristopher\Documentos\ECOMMERCE\VS\P1\Proyecto_1\Proyecto_1\Models\Modelo.edmx'. 
+﻿// La generación de código T4 está habilitada para el modelo 'C:\Users\pablo\Source\Repos\Cristopher240598\LlanterasHalcon\Proyecto_1\Models\Modelo.edmx'. 
 // Para habilitar la generación de código heredada, cambie el valor de la propiedad del diseñador 'Estrategia de generación de código'
 // por 'ObjectContext heredado'. Esta propiedad está disponible en la ventana Propiedades cuando se abre
 // el modelo en el diseñador.
