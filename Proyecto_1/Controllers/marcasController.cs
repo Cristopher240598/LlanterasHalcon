cfd4bb4cf000d1629cc3298baf95aa110741c0a5
Ivan Santana Santana <isantanas@toluca.tecnm.mx>
2020-12-31 17:58:21 -0600

    Marcas Vistas

---
+++ Proyecto_1/Controllers/marcasController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Proyecto_1.Models;
+
+namespace Proyecto_1.Controllers
+{
+    public class marcasController : Controller
+    {
+        private contextLlantera db = new contextLlantera();
+
+        // GET: marcas
+        public ActionResult Index()
+        {
+            return View(db.marcas.ToList());
+        }
+
+        // GET: marcas/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            marcas marcas = db.marcas.Find(id);
+            if (marcas == null)
+            {
+                return HttpNotFound();
+            }
+            return View(marcas);
+        }
+
+        // GET: marcas/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: marcas/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,nombre,imagen")] marcas marcas)
+        {
+            if (ModelState.IsValid)
+            {
+                db.marcas.Add(marcas);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(marcas);
+        }
+
+        // GET: marcas/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            marcas marcas = db.marcas.Find(id);
+            if (marcas == null)
+            {
+                return HttpNotFound();
+            }
+            return View(marcas);
+        }
+
+        // POST: marcas/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,nombre,imagen")] marcas marcas)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(marcas).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(marcas);
+        }
+
+        // GET: marcas/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            marcas marcas = db.marcas.Find(id);
+            if (marcas == null)
+            {
+                return HttpNotFound();
+            }
+            return View(marcas);
+        }
+
+        // POST: marcas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            marcas marcas = db.marcas.Find(id);
+            db.marcas.Remove(marcas);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
