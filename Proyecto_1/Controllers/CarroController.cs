7070daa1a835d3947ddf17b464fd458679e56919
pablofranco12 <walliwest12@gmail.com>
2021-01-14 22:36:25 -0600

    Avance de carro

--- Proyecto_1/Controllers/CarroController.cs
+++ Proyecto_1/Controllers/CarroController.cs
@@ -13,5 +13,10 @@ namespace Proyecto_1.Controllers
         {
             return View();
         }
+
+        public ActionResult Agregar()
+        {
+            return View();
+        }
     }
 }
